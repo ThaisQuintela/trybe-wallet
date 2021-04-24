d9f39fac979b3e33b08f23826d26ecbe96cacbd8
Thais Quintela <thais.s.quintela@gmail.com>
2021-04-24 01:41:14 -0300

    Updates

---
+++ precache-manifest.29bb3f1fcf1aff9207b2442e62f9ab6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4c59a32bedec534471aa2d3dd18ef1",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "c37d2c068d5c2a566a39",
+    "url": "/trybe-wallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "936fe88abaffed45e834",
+    "url": "/trybe-wallet/static/js/2.bcf2cbf7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.bcf2cbf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37d2c068d5c2a566a39",
+    "url": "/trybe-wallet/static/js/main.6dbcedcd.chunk.js"
+  },
+  {
+    "revision": "59d3c1ec903278203d08",
+    "url": "/trybe-wallet/static/js/runtime-main.2e2bea36.js"
+  }
+]);
